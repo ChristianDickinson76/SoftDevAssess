df7b80aec72f7e224a7ef4509c4f07c4912a3876
Christian Dickinson <christiandickinson76@gmail.com>
2026-01-29 21:19:32 +0000

    Added Week_6

---
+++ Week_6.py
@@ -0,0 +1,7 @@
+students = ["Amir", "Sarah", "Tom", "Aisha", "Leo"]
+students.append("Shaggy")
+students.append("Scooby")
+students.pop()
+students.insert(0, "Cheesy")
+students.sort()
+print("Current students are ", students)
